3d6ace52d0b5c8092abcad70f190650cdc138973
babamovandrej <andrejbabamov@gmail.com>
2025-08-11 10:33:24 +0200

    Add dynamic route for address

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './styles/App.css'
 import '@concero/ui-kit/styles/concero/index.css'
 import { Routes, Route } from 'react-router-dom'
 import { Header } from './components'
-import { Home } from './pages'
+import { Home, Address } from './pages'
 
 function App() {
 	return (
@@ -10,7 +10,7 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/message/:id" element={<></>} />
+				<Route path="/address/:address" element={<Address/>} />
 			</Routes>
 		</>
 	)
--- src/pages/message/Message.tsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from 'react-router-dom'
-
-export const Message = () => {
-	const { id } = useParams()
-	return <h2> Message Page. ID: {id}</h2>
-}
