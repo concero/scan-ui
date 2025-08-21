6dd5c0ee50110b4084e53fb8bed4c0e0c78fc8e6
babamovandrej <andrejbabamov@gmail.com>
2025-08-21 15:20:24 +0200

    Create data to populate the table

--- src/components/common/DataTable/DataTable.tsx
+++ src/components/common/DataTable/DataTable.tsx
@@ -1,12 +1,16 @@
 import type { ReactElement } from 'react'
 import type { Column } from '../TableRow'
 import { Table } from '../Table'
+import { TimeData, DirectionData, MessageData } from './Data/Data'
 import './styles.pcss'
+import { TransactionLabel } from '../TransactionLabel'
+import { TransactionType } from '@/pages'
+import { StatusLabel } from '../StatusLabel'
 
 type MessageRow = {
-	messageId: string
+	messageId: ReactElement
 	type: ReactElement
-	age: string
+	age: ReactElement
 	from: ReactElement
 	to: ReactElement
 	status: ReactElement
@@ -23,21 +27,21 @@ const columns: Column<MessageRow>[] = [
 
 const rows: MessageRow[] = [
 	{
-		messageId: '0x1673f72a...',
-		type: <>Canonical Bridge</>,
-		age: '1 Second ago',
-		from: <>Canonical Bridge</>,
-		to: <>space/space-1</>,
-		status: <span style={{ color: 'green' }}>Success</span>,
+		messageId: <MessageData messageId="0x4e3b2f1a7c8d9e0b6f5a1c2d3e4f567890abcdef1234567890abcdef123456789" />,
+		type: <TransactionLabel size='s' type={TransactionType.CanonicalBridge} />,
+		age: <TimeData time={'1 second ago'} />, 
+		from: <DirectionData chainLogo='https://api.concero.io/static/icons/chains/137.svg' address='0x239d5b78680e9AD600Ab41E56508670BA9E78F51' />,
+		to: <DirectionData   chainLogo='https://api.concero.io/static/icons/chains/137.svg' address='0x239d5b78680e9AD600Ab41E56508670BA9E78F51'/>,
+		status: <StatusLabel status='Success' size='m'/>,
 	},
-	{
-		messageId: '0xabcde123...',
-		type: <>IOU</>,
-		age: '15 Seconds ago',
-		from: <>space/space-2</>,
-		to: <>space/space-2</>,
-		status: <span style={{ color: 'orange' }}>Pending</span>,
-	},
+	// {
+	// 	messageId: '0xabcde123...',
+	// 	type: <>IOU</>,
+	// 	age: '15 Seconds ago',
+	// 	from: <>space/space-2</>,
+	// 	to: <>space/space-2</>,
+	// 	status: <span style={{ color: 'orange' }}>Pending</span>,
+	// },
 ]
 
 export const DataTable = (): ReactElement => {
