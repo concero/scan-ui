cd22fa6aae9b700419d525d97990f123fcf86891
babamovandrej <andrejbabamov@gmail.com>
2025-08-22 16:45:22 +0200

    Fix Desktop visuals

--- src/components/home/HomeVisuals/DesktopVisuals/DesktopVisuals.tsx
+++ src/components/home/HomeVisuals/DesktopVisuals/DesktopVisuals.tsx
@@ -1,27 +1,34 @@
 import type { ReactElement } from 'react'
 import type { Visual } from '../HomeVisuals'
+import { useSettingsStore } from '@/hooks'
 import './styles.pcss'
 
 const visuals: Visual[] = [
-	{ src: '/Home/visual_one.webp', alt: 'Flower', className: 'visual_one' },
-	{ src: '/Home/visual_two.webp', alt: 'Code', className: 'visual_two' },
-	{ src: '/Home/visual_three.webp', alt: 'Math', className: 'visual_three' },
-	{ src: '/Home/visual_four.webp', alt: 'File', className: 'visual_four' },
-	{ src: '/Home/visual_five.webp', alt: 'Disc', className: 'visual_five' },
+  { src: 'visual_one.webp', alt: 'Flower', className: 'visual_one' },
+  { src: 'visual_two.webp', alt: 'Code', className: 'visual_two' },
+  { src: 'visual_three.webp', alt: 'Math', className: 'visual_three' },
+  { src: 'visual_four.webp', alt: 'File', className: 'visual_four' },
+  { src: 'visual_five.webp', alt: 'Disc', className: 'visual_five' },
 ]
 
-export const DesktopVisuals = (): ReactElement => (
-	<div className="desktop_visuals">
-		{visuals.map(({ src, alt, className, props }) => (
-			<img
-				key={className}
-				src={src}
-				alt={alt}
-				className={`desktop_visual ${className}`}
-				draggable={false}
-				loading="lazy"
-				{...props}
-			/>
-		))}
-	</div>
-)
+export const DesktopVisuals = (): ReactElement => {
+  const { theme } = useSettingsStore()
+
+  const basePath = theme === 'light' ? '/Home/Light/' : '/Home/Dark/'
+
+  return (
+    <div className="desktop_visuals" data-theme={theme}>
+      {visuals.map(({ src, alt, className, props }) => (
+        <img
+          key={className}
+          src={`${basePath}${src}`}
+          alt={alt}
+          className={`desktop_visual ${className}`}
+          draggable={false}
+          loading="lazy"
+          {...props}
+        />
+      ))}
+    </div>
+  )
+}
