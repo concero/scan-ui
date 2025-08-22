d763df73cdb341b024efb85ee417be3b81cc2575
babamovandrej <andrejbabamov@gmail.com>
2025-08-22 17:07:58 +0200

    Finish tablet illustrations

--- src/components/home/HomeVisuals/TabletVisuals/TabletVisuals.tsx
+++ src/components/home/HomeVisuals/TabletVisuals/TabletVisuals.tsx
@@ -1,22 +1,26 @@
-import type { ReactElement } from "react"
-import type { Visual } from "../HomeVisuals"
-import "./styles.pcss"
+import type { ReactElement } from 'react'
+import type { Visual } from '../HomeVisuals'
+import { useSettingsStore } from '@/hooks'
+import './styles.pcss'
 
 const visuals: Visual[] = [
-    { src: '/Home/visual_one.svg', alt: 'Flower', className: 'visual_one' },
-    { src: '/Home/visual_two.svg', alt: 'Code', className: 'visual_two' },
-    { src: '/Home/visual_three.svg', alt: 'Math', className: 'visual_three' },
-    { src: '/Home/visual_four.svg', alt: 'File', className: 'visual_four' },
-    { src: '/Home/visual_five.svg', alt: 'Disc', className: 'visual_five' },
+    { src: 'visual_one.webp', alt: 'Flower', className: 'visual_one' },
+    { src: 'visual_two.webp', alt: 'Code', className: 'visual_two' },
+    { src: 'visual_three.webp', alt: 'Math', className: 'visual_three' },
+    { src: 'visual_four.webp', alt: 'File', className: 'visual_four' },
+    { src: 'visual_five.webp', alt: 'Disc', className: 'visual_five' },
 ]
 
 export const TabletVisuals = (): ReactElement => {
+    const { theme } = useSettingsStore()
+    const basePath = theme === 'light' ? '/Home/Light/' : '/Home/Dark/'
+
     return (
-        <div className="tablet_visuals">
+        <div className="tablet_visuals" data-theme={theme}>
             {visuals.map(({ src, alt, className, props }) => (
                 <img
                     key={className}
-                    src={src}
+                    src={`${basePath}${src}`}
                     alt={alt}
                     className={`tablet_visual ${className}`}
                     draggable={false}
