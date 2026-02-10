3f48061c2a6258cab01ca9efcff68f771ee2fa2d
babamovandrej <andrejbabamov@gmail.com>
2026-02-10 12:58:13 +0100

    Expand chains support to include mainnet deployments

--- src/hooks/Loadables/useLoadChains.ts
+++ src/hooks/Loadables/useLoadChains.ts
@@ -4,23 +4,35 @@ import { useChainsStore } from '../useChainsStore'
 import { getChains, toConceroChains } from '@/utils/chains'
 
 export const useLoadChains = () => {
-	const { setChains, setLoading } = useChainsStore()
+    const { setChains, setLoading } = useChainsStore()
 
-	const { data: chains, isLoading: isLoading } = useQuery({
-		queryKey: ['chainsConfig'],
-		queryFn: async () => {
-			const response = await getChains()
-			return toConceroChains(response.payload.items)
-		},
-		staleTime: 30_000,
-		retry: 2,
-		refetchOnWindowFocus: false,
-	})
+    const { data: chains, isLoading, error } = useQuery({
+        queryKey: ['chainsConfig'],
+        queryFn: async () => {
+            const [mainnetResponse, testnetResponse] = await Promise.all([
+                getChains(false), 
+                getChains(true),
+            ])
 
-	useEffect(() => {
-		setChains(chains || [])
-		setLoading(isLoading)
-	}, [chains, isLoading, setChains, setLoading])
+            const mainnetChains = toConceroChains(mainnetResponse.payload.items)
+            const testnetChains = toConceroChains(testnetResponse.payload.items)
+            
+            return [...mainnetChains, ...testnetChains]
+        },
+        staleTime: 30_000,
+        retry: 2,
+        refetchOnWindowFocus: false,
+    })
 
-	return { chains: chains, loading: isLoading }
+    useEffect(() => {
+        setChains(chains || [])
+		console.log('Chains loaded:', chains)
+        setLoading(isLoading)
+    }, [chains, isLoading, setChains, setLoading])
+
+    return { 
+        chains: chains || [], 
+        loading: isLoading,
+        error 
+    }
 }
