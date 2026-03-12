fee589ab9f07b8dc124ee20fe6d957658b880e8b
babamovandrej <andrejbabamov@gmail.com>
2026-03-12 11:32:25 +0100

    Remove security headers

--- src/components/common/MetaTags/MetaTags.tsx
+++ src/components/common/MetaTags/MetaTags.tsx
@@ -1,6 +1,5 @@
 import { memo } from 'react'
 import { Helmet } from 'react-helmet-async'
-import { ContentSecurityPolicy } from '@/configuration/policy'
 
 const DEFAULTS = {
 	TITLE: 'Concero | Scan',
@@ -37,7 +36,6 @@ export const MetaTags = memo(
 		additionalMeta,
 	}: MetaTagsProps) => (
 		<Helmet>
-			<meta httpEquiv="Content-Security-Policy" content={ContentSecurityPolicy} />
 			<title>{title}</title>
 			<meta name="description" content={description} />
 			<meta name="keywords" content={DEFAULTS.KEYWORDS} />
